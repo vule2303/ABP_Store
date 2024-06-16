2a1567e6270735910704f984141a3b791f369ae4
Vu <leductuanvu5@gmail.com>
2024-06-16 17:21:43 +0700

    feat: add home page for public application

---
+++ aspnet-core/src/Store.Public.Web/Pages/Shared/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Store.Public.Web.Pages.Shared
+{
+    public class _LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
